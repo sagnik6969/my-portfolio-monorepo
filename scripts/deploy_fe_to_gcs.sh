d75f0b036349482932f2d136711118d5164afda1
Sagnik Jana <sagnikjana2001@gmail.com>
2025-10-06 17:15:09 +0530

    updated deployment script

--- scripts/deploy_fe_to_gcs.sh
+++ scripts/deploy_fe_to_gcs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 echo "Deploying $DIST_FOLDER to gs://$FE_BUCKET_NAME..."
 
 # Use gsutil to sync the dist folder with the GCS bucket.
 # -m: performs a parallel (multi-threaded/multi-processing) copy.
 # -r: recurses into directories.
-# The final "/" on the source directory is important to copy the contents, not the directory itself.
+# The final "/*" on the source directory is important to copy the contents, not the directory itself.
 gsutil -m rsync -r "$DIST_FOLDER/*" "gs://$FE_BUCKET_NAME/my_portfolio"
 
 echo "Deployment to gs://$FE_BUCKET_NAME complete."
