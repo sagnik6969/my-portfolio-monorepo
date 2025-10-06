1a7372e0ddf58ed56050ea2b74a4c26b93d850eb
Sagnik Jana <sagnikjana2001@gmail.com>
2025-10-06 17:41:28 +0530

    updated the deploy script

--- scripts/deploy_fe_to_gcs.sh
+++ scripts/deploy_fe_to_gcs.sh
@@ -13,7 +13,7 @@ echo "Deploying $DIST_FOLDER to gs://$GCP_FE_BUCKET_NAME..."
 # -m: performs a parallel (multi-threaded/multi-processing) copy.
 # -r: recurses into directories.
 # The final "/*" on the source directory is important to copy the contents, not the directory itself.
-gsutil -m rsync -r "$DIST_FOLDER/*" "gs://$GCP_FE_BUCKET_NAME/my_portfolio"
+gsutil -m rsync -r "$DIST_FOLDER" "gs://$GCP_FE_BUCKET_NAME/my_portfolio"
 
 echo "Deployment to gs://$GCP_FE_BUCKET_NAME complete."
 echo "Your application should be available at: https://storage.googleapis.com/$GCP_FE_BUCKET_NAME/index.html"
